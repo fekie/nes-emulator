dbe6fbb0bb381ceeea0f2d730ab96049d5a94fe1
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2024-05-22 18:57:24 -0400

    chore: add memory mapping documentation

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -2,7 +2,20 @@ use crate::ppu::PPU;
 use std::cell::RefCell;
 use std::rc::Rc;
 
+#[derive(Debug, Clone, Copy)]
 pub struct ProcessorStatus(u8);
+
+impl ProcessorStatus {
+    fn set_carry(&mut self) {
+        let mask = 0b0000_0001;
+        self.0 |= mask;
+    }
+
+    fn clear_carry(&mut self) {
+        let mask = 0b1111_1110;
+        self.0 &= mask;
+    }
+}
 
 pub struct Cpu {
     accumulator_register: u8,
@@ -15,6 +28,24 @@ pub struct Cpu {
     memory_mapper: CpuMemoryMapper,
 }
 
+// We use 2KB of work ram.
+pub struct WorkRAM([u8; 0x800]);
+
+/// Memory Map:
+///
+/// | Address range |  Size  |                                  Device                                  |   |   |
+/// |:-------------:|:------:|:------------------------------------------------------------------------:|---|---|
+/// | $0000–$07FF   | $0800  | 2 KB internal RAM                                                        |   |   |
+/// | $0800–$0FFF   | $0800  | Mirrors of $0000–$07FF                                                   |   |   |
+/// | $1000–$17FF   | $0800  |                                                                          |   |   |
+/// | $1800–$1FFF   | $0800  |                                                                          |   |   |
+/// | $2000–$2007   | $0008  | NES PPU registers                                                        |   |   |
+/// | $2008–$3FFF   | $1FF8  | Mirrors of $2000–$2007 (repeats every 8 bytes)                           |   |   |
+/// | $4000–$4017   | $0018  | NES APU and I/O registers                                                |   |   |
+/// | $4018–$401F   | $0008  | APU and I/O functionality that is normally disabled. See CPU Test Mode.  |   |   |
+/// | $4020–$FFFF   | $BFE0  | Unmapped. Available for cartridge use.                                   |   |   |
+/// | *$6000-$7FFF  | $2000  | Usually cartridge RAM, when present.                                     |   |   |
+/// | *$8000-$FFFF  | $8000  | Usually cartridge ROM and mapper registers.                              |   |   |
 pub struct CpuMemoryMapper {
-    foo: Rc<RefCell<PPU>>,
+    ppu: Rc<RefCell<PPU>>,
 }
