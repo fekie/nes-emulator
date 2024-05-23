f80723f60976b3623160d99a7d15303968326d8a
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2024-05-22 21:18:33 -0400

    chore: it compiles now

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -16,16 +16,17 @@ impl Cpu {
             self.processor_status.set_carry_flag()
         }
     }
+
     pub fn AND(&mut self, presumed_mem_byte: u8) {
         let _ = self.accumulator_register & presumed_mem_byte;
     }
-  
+
     pub fn ASL(&mut self, presumed_mem_byte: u8) {}
 
     pub fn JMP(&mut self, presumed_argument: u8) {
         self.program_counter = presumed_argument.into();
     }
-  
+
     pub fn BCC(&mut self) {
         if !self.processor_status.carry_flag() {
             self.program_counter;
@@ -35,25 +36,30 @@ impl Cpu {
     // all the instructions starting with T are implied addressing
     pub fn TAX(&mut self) {}
 
-
     pub fn CLC(&mut self) {
         self.processor_status.clear_carry_flag();
     }
+
     pub fn CLD(&mut self) {
         self.processor_status.clear_decimal_flag();
     }
+
     pub fn CLI(&mut self) {
         self.processor_status.clear_interrupt_disable_flag();
     }
+
     pub fn CLV(&mut self) {
         self.processor_status.clear_over_flag();
     }
+
     pub fn SEC(&mut self) {
         self.processor_status.set_carry_flag();
     }
+
     pub fn SED(&mut self) {
         self.processor_status.set_decimal_flag();
     }
+
     pub fn SEI(&mut self) {
         self.processor_status.set_interrupt_disable_flag();
     }
