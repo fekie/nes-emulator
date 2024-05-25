c4dade5bee17ccbdc03c39f4124069d3c3c5db5c
FBIGlowie <144389458+FBIGlowie@users.noreply.github.com>
2024-05-25 14:21:39 -0400

    Addressing is confusing

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -1,4 +1,65 @@
 use super::Cpu;
+use super::AddressingMode::{self, *};
+pub enum Instructions { // fekie you can add some documentation here if you want, I aint even gonna bother
+    ADC{adr: AddressingMode},
+    AND{adr: AddressingMode},
+    ASL{adr: AddressingMode},
+    BCC{adr: AddressingMode},
+    BCS{adr: AddressingMode},
+    BEQ{adr: AddressingMode},
+    BIT{adr: AddressingMode},
+    BMI{adr: AddressingMode},
+    BNE{adr: AddressingMode},
+    BPL{adr: AddressingMode},
+    BRK{adr: AddressingMode},
+    BVC{adr: AddressingMode},
+    BVS{adr: AddressingMode},
+    CLC{adr: AddressingMode},
+    CLD{adr: AddressingMode},
+    CLI{adr: AddressingMode},
+    CLV{adr: AddressingMode},
+    CMP{adr: AddressingMode},
+    CPX{adr: AddressingMode},
+    CPY{adr: AddressingMode},
+    DEC{adr: AddressingMode},
+    DEx{adr: AddressingMode},
+    DEY{adr: AddressingMode},
+    EOR{adr: AddressingMode},
+    INC{adr: AddressingMode},
+    INX{adr: AddressingMode},
+    INY{adr: AddressingMode},
+    JMP{adr: AddressingMode},
+    JSR{adr: AddressingMode},
+    LDA{adr: AddressingMode},
+    LDX{adr: AddressingMode},
+    LDY{adr: AddressingMode},
+    LSR{adr: AddressingMode},
+    NOP{adr: AddressingMode},
+    ORA{adr: AddressingMode},
+    PHA{adr: AddressingMode},
+    PHP{adr: AddressingMode},
+    PLA{adr: AddressingMode},
+    PLP{adr: AddressingMode},
+    ROL{adr: AddressingMode},
+    ROR{adr: AddressingMode},
+    RTI{adr: AddressingMode},
+    RTD{adr: AddressingMode},
+    SBC{adr: AddressingMode},
+    SEC{adr: AddressingMode},
+    SED{adr: AddressingMode},
+    SEI{adr: AddressingMode},
+    STA{adr: AddressingMode},
+    STX{adr: AddressingMode},
+    STY{adr: AddressingMode},
+    TAX{adr: AddressingMode},
+    TAY{adr: AddressingMode},
+    TSX{adr: AddressingMode},
+    TXA{adr: AddressingMode},
+    TXS{adr: AddressingMode},
+    TYA{adr: AddressingMode},
+
+}
+
 
 impl Cpu {
     pub fn ADC(&mut self, presumed_mem_byte: u8) {
@@ -19,13 +80,13 @@ impl Cpu {
     pub fn AND(&mut self, presumed_mem_byte: u8) {
         let _ = self.accumulator_register & presumed_mem_byte;
     }
-  
+
     pub fn ASL(&mut self, presumed_mem_byte: u8) {}
 
     pub fn JMP(&mut self, presumed_argument: u8) {
         self.program_counter = presumed_argument.into();
     }
-  
+
     pub fn BCC(&mut self) {
         if !self.processor_status.carry_flag() {
             self.program_counter;
@@ -33,7 +94,24 @@ impl Cpu {
     }
 
     // all the instructions starting with T are implied addressing
-    pub fn TAX(&mut self) { }
+    pub fn TAX(&mut self) {
+        self.x_register = self.accumulator_register;
+    }
+    pub fn TAY(&mut self) {
+        self.y_register = self.accumulator_register;
+    }
+    pub fn TSX(&mut self) {
+        self.x_register = self.stack_pointer;
+    }
+    pub fn TXA(&mut self) {
+        self.accumulator_register = self.x_register;
+    }
+    pub fn TXS(&mut self) {
+        self.stack_pointer = self.x_register;
+    }
+    pub fn TYA(&mut self) {
+        self.accumulator_register = self.y_register;
+    }
 
 
     pub fn CLC(&mut self) {
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -2,11 +2,14 @@ use crate::ppu::PPU;
 use std::cell::RefCell;
 use std::rc::Rc;
 mod instructions;
-
+use instructions::Instructions;
 #[derive(Debug, Clone, Copy, Default)]
 pub struct ProcessorStatus(u8);
 
 impl ProcessorStatus {
+    pub fn new() -> Self {
+        ProcessorStatus(0)
+    }
     pub fn carry_flag(&self) -> bool {
         self.0 & 0b0000_0001 != 0
     }
@@ -108,89 +111,6 @@ impl ProcessorStatus {
     }
 }
 
-impl ProcessorStatus {
-    pub fn new() -> Self {
-        ProcessorStatus(0)
-    }
-    pub fn carry_flag(&self) -> bool {
-        return self.0 & 0b0000_0001 != 0;
-    }
-    pub fn set_carry_flag(&mut self) {
-        let mask = 0b0000_0001;
-        self.0 |= mask;
-    }
-    pub fn clear_carry_flag(&mut self) {
-        let mask = 0b1111_1110;
-        self.0 &= mask;
-    }
-    pub fn zero_flag(&self) -> bool {
-        return self.0 & 0b0000_0010 != 0;
-    }
-    pub fn set_zero_flag(&mut self) {
-        let mask = 0b0000_0010;
-        self.0 |= mask;
-    }
-    pub fn clear_zero_flag(&mut self) {
-        let mask = 0b1111_1101;
-        self.0 &= mask;
- 
-    }
-    pub fn inter_flag(&self) -> bool {
-        return self.0 & 0b0000_0100 != 0;
-    }
-    pub fn set_inter_flag(&mut self) {
-        let mask = 0b0000_0100;
-        self.0 |= mask;
-    }
-    pub fn clear_inter_flag(&mut self) {
-        let mask = 0b1111_1011;
-        self.0 &= mask;
-    }
-    pub fn deci_flag(&self) -> bool {
-        return self.0 & 0b0000_1000 != 0;
-    }
-    pub fn set_deci_flag(&mut self) {
-        let mask = 0b0000_1000;
-        self.0 |= mask;
-    }
-    pub fn clear_deci_flag(&mut self) {
-        let mask = 0b1111_0111;
-        self.0 &= mask;
-    }
-    pub fn break_flag(&self) -> bool {
-        return self.0 & 0b0001_0000 != 0;
-    }
-    pub fn set_break_flag(&mut self) {
-        let mask = 0b0001_0000;
-        self.0 |= mask;
-    }
-    pub fn clear_break_flag(&mut self) {
-        let mask = 0b1110_1111;
-        self.0 &= mask;
-    }
-    pub fn over_flag(&self) -> bool {
-        return self.0 & 0b0100_0000 != 0;
-    }
-    pub fn set_over_flag(&mut self) {
-        let mask = 0b0100_0000;
-        self.0 |= mask;
-    }
-    pub fn clear_over_flag(&mut self) {
-        let mask = 0b1011_1111;
-        self.0 &= mask;
-    }
-    pub fn neg_flag(&self) -> bool {
-        return self.0 & 0b1000_0000 != 0;
-    }
-    pub fn set_neg_flag(&mut self) {
-        let mask = 0b1000_0000;
-        self.0 |= mask;
-    }
-    pub fn clear_neg_flag(&mut self) {
-        let mask = 0b0111_1111;
-        self.0 &= mask;
-    }
-}
 pub struct Cpu {
     accumulator_register: u8,
     x_register: u8,
@@ -240,6 +160,34 @@ impl CpuMemoryMapper {
     }
 }
 
+//https://emudev.de/nes-emulator/opcodes-and-addressing-modes-the-6502/   <-- good stuff
+//https://blogs.oregonstate.edu/ericmorgan/2022/01/21/6502-addressing-modes/  <--- also this too
+pub enum AddressingMode {
+    Relative{offset: u8},
+    Accumulator{accumulator: u8}, //takes 2 cycles to complete
+    Immediate { adr: u16 }, // 2 cycles
+    Implied, //no addressing mode, but takes 2 cycles
+    Zeropage { adr: u16 },
+    ZeropageXIndex { adr: u16, X: u8 },
+    ZeropageYIndex { adr: u16, Y: u8 },
+    IndirectXIndex { adr: u16, X: u8 },
+    IndirectYIndex { adr: u16, Y: u8 },
+    Absolute { adr: u16 }, // 4 cycles to complete, wierd stuff happens incrementing it beyond absolute address causing it to cross pages and requried another cpu cycle but this only applied below
+    AbsoluteXIndex { adr: u16, X: u8 }, // 4 cycles to complete, incremented by the value in the X register
+    AbsoluteYIndex { adr: u16, Y: u8 }, // 4 cycles to complete, incremented by the value in the Y register
+}
+
+pub fn decodeOPandADR(opcode: u8) -> Instructions { // just some sketches, can change later on
+    match opcode {
+        0x69 => Instructions::ADC { adr: AddressingMode::Implied },
+        0x65 => Instructions::ADC { adr: AddressingMode::Implied },
+        0x75 => ,
+        
+
+    }
+}
+
+
 #[test]
 fn test_flags() {
     let mut flag_reg = ProcessorStatus::new();
@@ -253,7 +201,7 @@ fn test_flags() {
         flag_reg.carry_flag()
     });
     assert!(!flag_reg.zero_flag());
-  
+
     assert!({
         flag_reg.set_zero_flag();
         flag_reg.zero_flag()
@@ -262,23 +210,23 @@ fn test_flags() {
         flag_reg.clear_zero_flag();
         flag_reg.zero_flag()
     });
-    assert!(!flag_reg.inter_flag());
+    assert!(!flag_reg.interrupt_disable_flag());
     assert!({
-        flag_reg.set_inter_flag();
-        flag_reg.inter_flag()
+        flag_reg.set_interrupt_disable_flag();
+        flag_reg.interrupt_disable_flag()
     });
     assert!(!{
-        flag_reg.clear_inter_flag();
-        flag_reg.inter_flag()
+        flag_reg.clear_interrupt_disable_flag();
+        flag_reg.interrupt_disable_flag()
     });
-    assert!(!flag_reg.deci_flag());
+    assert!(!flag_reg.decimal_flag());
     assert!({
-        flag_reg.set_deci_flag();
-        flag_reg.deci_flag()
+        flag_reg.set_decimal_flag();
+        flag_reg.decimal_flag()
     });
     assert!(!{
-        flag_reg.clear_deci_flag();
-        flag_reg.deci_flag()
+        flag_reg.clear_decimal_flag();
+        flag_reg.decimal_flag()
     });
     assert!(!flag_reg.break_flag());
     assert!({
